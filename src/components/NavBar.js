5895a9df4689d6f35d2ef749150d7e2e1b8bd2b7
Amal K K <kkamalkk1994@gmail.com>
2022-08-30 21:22:13 +0530

    removing non functional links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,11 @@
-import classes from "./NavBar.module.css";
+import classes from "./NavBar.module.css"
 
 const NavBar = () => {
   return (
     <nav className={classes.navBar}>
       <h1>MY VIDEOS</h1>
-      <div className={classes.navLinks}>
-        <a href="./">
-          <h4>Home</h4>
-        </a>
-        <a href="./">
-          <h4>Recent Uploads</h4>
-        </a>
-        <a href="./">
-          <h4>All Videos</h4>
-        </a>
-        <a href="./">
-          <h4>About</h4>
-        </a>
-        <a href="./">
-          <h4>Contact</h4>
-        </a>
-      </div>
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
